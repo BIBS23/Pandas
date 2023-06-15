98d4442d00d56b04b6605f9c6452dfb35fa614c7
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-06-15 15:27:02 +0530

    Create find_greater_csv.py

---
+++ find_greater_csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+dict = {
+    'Reg No' : ['ABC123','ECH256','FET235','ECS042','CSE069'],
+    'Name' : ["Bibin","Abin","Ajay","Ajai","Joseph"],
+    'Semester' : ["S5","S8","S2","S4","S6"],
+    'College' : ["ABC","ECH","FET","ECS","CSE"],
+    'CGPA' : [8,7,6.5,9.1,9.5]
+
+}
+
+df = pd.DataFrame(dict)
+
+df.to_csv('toppers.csv')
+mylist = pd.read_csv('toppers.csv')
+
+sort = mylist[mylist['CGPA']>9]
+
+print(sort)
